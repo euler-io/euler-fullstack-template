9b64f279746ed4cd531b267ce42c1a3f086455f9
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2021-04-24 23:02:21 -0300

    Deploy

--- {{cookiecutter.project_slug}}/deploy/hooks/pre_gen_project.sh
+++ {{cookiecutter.project_slug}}/deploy/hooks/pre_gen_project.sh
@@ -5,4 +5,23 @@
 #{% endif %}
 
 echo "Generated JWT Secret '{{ cookiecutter.jwt_secret }}'."
+
+#{% if cookiecutter.admin_password == "random" %}
+#{{ cookiecutter.update({"admin_password": random_ascii_string(12) }) }}
+#{% endif %}
+
+echo "Generated admin password '{{ cookiecutter.admin_password }}'."
+
+#{% if cookiecutter.apiadmin_password == "random" %}
+#{{ cookiecutter.update({"apiadmin_password": random_ascii_string(12) }) }}
+#{% endif %}
+
+echo "Generated apiadmin password '{{ cookiecutter.apiadmin_password }}'."
+{% endraw %}
+
+#{% if cookiecutter.kibana_password == "random" %}
+#{{ cookiecutter.update({"kibana_password": random_ascii_string(12) }) }}
+#{% endif %}
+
+echo "Generated kibana password '{{ cookiecutter.kibana_password }}'."
 {% endraw %}
